679cae37aff8bdccda09e2531b5953552150260b
haritsaardli <haritsaardli@gmail.com>
2022-02-14 14:54:09 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# final-project-2-group-2
+
+## jangan lupa ganti branch
---
+++ asset/js/index.js
@@ -0,0 +1,6 @@
+$(function () {
+	$("#section-A").load("./asset/pages/Section-A.html");
+	$("#section-B").load("./asset/pages/Section-B.html");
+	$("#section-C").load("./asset/pages/Section-C.html");
+	$("#section-D").load("./asset/pages/Section-D.html");
+});
